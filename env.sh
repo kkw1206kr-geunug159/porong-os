1d8096e1d9c38b539bb12b62fb2fe6f26947994c
Pororo <kkw1206kr@kakao.com>
2024-07-21 17:05:35 +0900

    auto

---
+++ env.sh
@@ -0,0 +1,12 @@
+export WORKING=~/porong-os
+
+alias d="clear;cd $WORKING;tree"
+
+alias home-html="vim $WORKING/homescreen.html"
+alias home-css="vim $WORKING/css/home.css"
+alias home-js="vim $WORKING/js/home.js"
+
+alias run="i3 & firefox ~/porong-os/homescreen.html &"
+
+alias pull="cd $WORKING;git pull"
+alias push="cd $WORKING;git add .;git commit -m 'auto';git push"
