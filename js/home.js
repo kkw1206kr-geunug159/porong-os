a9a6e050d54247258cb65f446ca34146b1b78353
Pororo <kkw1206kr@kakao.com>
2024-07-23 11:21:28 +0900

    auto

--- js/home.js
+++ js/home.js
@@ -8,19 +8,32 @@ function sleep(sec) {
     }
 }
 
+const openingElement=[
+	{transform:"scale(0)"},
+	{transform:"scale(1)"},
+];
+const closingElement=[
+	{transform:"scale(1)"},
+	{transform:"scale(0)"},
+];
+const animationDuration={
+	duration:250,
+	iteration:1,
+};
+
 function closeCtrlPanAnim(){
 	document.getElementById("controlCenter").style.animationName="closeCtrlPan";
 }
 
 function ctrlPan(){
 	if(ifCtrlPanOpened==1){
-		setTimeout(closeCtrlPanAnim,500)
-		document.getElementById("controlCenter").style.display="none";
+		document.getElementById("controlCenter").animate(closingElement,animationDuration);
+	document.getElementById("controlCenter").style.transform="scale(0)";
 		ifCtrlPanOpened=0;
 	}
 	else{
-		document.getElementById("controlCenter").style.display="block";
-		document.getElementById("controlCenter").style.animationName="openCtrlPan";
+		document.getElementById("controlCenter").animate(openingElement,animationDuration);
+	document.getElementById("controlCenter").style.transform="scale(1)";
 		ifCtrlPanOpened=1;
 	}
 }
