fe701fd813bc58837a4eefcc96155f2155d38000
Pororo <kkw1206kr@kakao.com>
2024-07-24 18:33:17 +0900

    auto

---
+++ homeScreen.js
@@ -0,0 +1,6 @@
+const wallpaper=document.getElementById("wallpaper");
+
+function setWallpaper(){
+	wallpaper.style.backgroundColor="red";
+	wallpaper.style.backgroundImage="url('./wallpaper.jpg')";
+}
